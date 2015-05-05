8e9c399c9df40feeae727f232af19a52516342a5
Yord <philipp.wille@web.de>
2015-05-05 11:01:30 +0200

    Added config

--- src/main/java/Template.java
+++ src/main/java/Template.java
@@ -1,9 +1,14 @@
 package de.tu_bs.cs.ifis.sqlgame;
 
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.Config;
+
 public class Template {
     
     public static void main(String[] args) {
-        System.out.println("Template yay");
+        Config conf = ConfigFactory.load();
+        String welcometext = conf.getString("template.welcometext");
+        System.out.println(welcometext);
     }
     
 }
